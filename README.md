d689a630b1b7f498719bacb40fab6d089de7c4fa
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-03-28 07:08:56 +0200

    Add initial project files with gallery functionality and styling

---
+++ README.md
@@ -0,0 +1,2 @@
+You can view the live version of the tasks on the following page:
+[Live Demo](https://antmuraha.github.io/goit-js-hw-14/)
---
+++ js/gallery.js
@@ -0,0 +1,70 @@
+import basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/+esm';
+
+async function fetchImages() {
+  // Simulating a fetch of images
+  try {
+    const { default: images } = await import('./images.js');
+    console.log(images);
+    return images;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    return [];
+  }
+}
+
+async function initGallery() {
+  const images = await fetchImages();
+
+  const galleryEl = document.querySelector('.gallery');
+  const template = document.querySelector('#gallery-item-template');
+  const fragment = document.createDocumentFragment();
+
+  images.forEach(({ preview, original, description }) => {
+    const clone = template.content.cloneNode(true);
+    const link = clone.querySelector('a.gallery-link');
+    const img = clone.querySelector('img.gallery-image');
+
+    link.href = original;
+    img.src = preview;
+    img.alt = description;
+    img.dataset.source = original;
+
+    fragment.appendChild(clone);
+  });
+  galleryEl.appendChild(fragment);
+
+  let currentIndex = -1;
+
+  galleryEl?.addEventListener('click', event => {
+    event.preventDefault();
+    const target = event.target.closest('img.gallery-image');
+    if (!target) return;
+
+    currentIndex = images.findIndex(
+      image => image.original === target.dataset.source
+    );
+
+    const showLightbox = index => {
+      const { original, description } = images[index];
+      const instance = basicLightbox.create(
+        `
+      <div class="lightbox">
+        <img class="lightbox-image" src="${original}" alt="${description}" />
+      </div>
+      `,
+        {
+          onShow: instance => {
+            console.log('Lightbox opened', instance.element());
+            instance.element().addEventListener('click', instance.close);
+          },
+        }
+      );
+
+      instance.show();
+    };
+
+    showLightbox(currentIndex);
+  });
+}
+
+initGallery();
